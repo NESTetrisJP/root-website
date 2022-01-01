fb8b0d51674e10a77692d131f8626c1d0c88d10e
suitougreentea <suitougreentea@users.noreply.github.com>
2022-01-01 14:23:46 +0900

    Fix apng tag

--- scripts/figure.js
+++ scripts/figure.js
@@ -1,4 +1,4 @@
 hexo.extend.tag.register("apng", (args) => {
-  return `<img class="apng-images" src=/images/apng/${args[0]}" />`
+  return `<img class="apng-images" src="/images/apng/${args[0]}" />`
 }, false)
 
