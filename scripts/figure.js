e7e6894b47ca48e6df1b468fe95da373048c9f3e
suitougreentea <suitougreentea@users.noreply.github.com>
2022-01-01 15:25:20 +0900

    Fix figure.js to support non-root config

--- scripts/figure.js
+++ scripts/figure.js
@@ -1,4 +1,4 @@
-hexo.extend.tag.register("apng", (args) => {
-  return `<img class="apng-images" src="/images/apng/${args[0]}" />`
+hexo.extend.tag.register("apng", function (args) {
+  return `<img class="apng-images" src="${hexo.config.root}images/apng/${args[0]}" />`
 }, false)
 
